8d9d963af31e9d1cd8aa3de5b0ae44982eb9371c
Raghav Dadhwal <raghavdadhwal07@gmail.com>
2021-09-29 12:40:13 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+// import Container from "./components/Container";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import NewContainer from "./components/NewContainer.jsx";
+import UpdateContainer from "./components/update.jsx";
+import HooksContainer from "./components/HooksContainer";
+import HooksUpdateContainer from "./components/HooksUpdate";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header>
+          <h1>React Skeletons</h1>
+        </header>
+        <div className="content">
+          <BrowserRouter>
+            <Switch>
+              {/* <Route path="/update/:id" component={UpdateContainer} /> */}
+              {/* <Route exact path="/" component={NewContainer} /> */}
+              <Route path="/update/:id" component={HooksUpdateContainer} />
+              <Route exact path="/" component={HooksContainer} />
+            </Switch>
+          </BrowserRouter>
+        </div>
+      </div>
+    </Provider>
   );
 }
 
